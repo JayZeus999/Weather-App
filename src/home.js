f20e76bf60d516133f3a5a02e54ee2681345b526
Jay Royce <faladejoseph400@gmail.com>
2026-05-13 20:59:46 +0100

    Strip down the template site

--- src/home.js
+++ src/home.js
@@ -1,30 +1,25 @@
 // src/home.js
 
 function loadHome() {
+  const container = document.querySelector("#content");
 
-    const container = document.querySelector("#content");
+  container.innerHTML = "";
 
-    container.innerHTML = "";
+  // const headline = document.createElement("h1");
+  // headline.classList.add("logo");
+  // headline.textContent = "Black Swan Nocturne";
 
-    const headline = document.createElement("h1");
-    headline.classList.add("logo");
-    headline.textContent = "Black Swan Nocturne";
+  // const tagline = document.createElement("h3");
+  // tagline.classList.add("classy-text")
+  // tagline.textContent = " Rare Obsidian Luxury ";
 
+  // const reserveBtn = document.createElement("button")
+  // reserveBtn.classList.add("reserveBtn");
+  // reserveBtn.textContent = "Reserve Your Table";
 
-    const tagline = document.createElement("h3");
-    tagline.classList.add("classy-text")
-    tagline.textContent = " Rare Obsidian Luxury ";
-
-
-    const reserveBtn = document.createElement("button")
-    reserveBtn.classList.add("reserveBtn");
-    reserveBtn.textContent = "Reserve Your Table";
-
-
-    container.appendChild(headline);
-    container.appendChild(tagline);
-    container.appendChild(reserveBtn);
+  // container.appendChild(headline);
+  // container.appendChild(tagline);
+  // container.appendChild(reserveBtn);
 }
-
 
 export { loadHome };
