5b3e1d77bc11c7f5fdf34c4a4ab37bd1a19bbfa8
Jay Royce <faladejoseph400@gmail.com>
2026-05-16 09:27:32 +0100

    Fetch API data & add event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 // src/index.js
 
 import "./styles.css";
+
+
+const weatherForm = document.querySelector("#weather-form");
+const locationInput = document.querySelector("#location-input");
+// const weatherResult = document.querySelector("#weather-result");
+
+
+async function getWeather(location) {
+  const apiKey = "TRSB28M3K6CGPVNY9Z42XGCTP";
+
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`
+  );
+
+  const weatherData = await response.json();
+
+  console.log(weatherData);
+
+}
+
+weatherForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const location = locationInput.value;
+
+  getWeather(location);
+});
+
+
+// async function getWeather(location) {
+//   const apiKey = "YOUR_API_KEY_HERE";
+
+//   const response = await fetch(
+//     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`
+//   );
+
+//   const weatherData = await response.json();
+
+//   console.log(weatherData);
+// }
+
+// weatherForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   const location = locationInput.value;
+
+//   getWeather(location);
+// });
