07bb30ab076be7f25b70a278931ae3b89f58b9a5
Jay Royce <faladejoseph400@gmail.com>
2026-05-16 09:58:56 +0100

    Center body

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@
 
 import "./styles.css";
 
-
 const weatherForm = document.querySelector("#weather-form");
 const locationInput = document.querySelector("#location-input");
 // const weatherResult = document.querySelector("#weather-result");
-
 
 async function getWeather(location) {
   const apiKey = "TRSB28M3K6CGPVNY9Z42XGCTP";
@@ -18,7 +16,6 @@ async function getWeather(location) {
   const weatherData = await response.json();
 
   console.log(weatherData);
-
 }
 
 weatherForm.addEventListener("submit", (event) => {
@@ -28,24 +25,3 @@ weatherForm.addEventListener("submit", (event) => {
 
   getWeather(location);
 });
-
-
-// async function getWeather(location) {
-//   const apiKey = "YOUR_API_KEY_HERE";
-
-//   const response = await fetch(
-//     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${apiKey}`
-//   );
-
-//   const weatherData = await response.json();
-
-//   console.log(weatherData);
-// }
-
-// weatherForm.addEventListener("submit", (event) => {
-//   event.preventDefault();
-
-//   const location = locationInput.value;
-
-//   getWeather(location);
-// });
