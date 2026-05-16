0fed1a0c0c53c6db9c294d44d473a5b49ebe785a
Jay Royce <faladejoseph400@gmail.com>
2026-05-16 08:38:49 +0100

    Remove home.js completely

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 // src/index.js
 
 import "./styles.css";
-
-import { loadHome } from "./home.js";
-
-const homeBtn = document.querySelector(".home-btn");
-homeBtn.addEventListener("click", () => {
-  loadHome();
-});
-
-loadHome();
